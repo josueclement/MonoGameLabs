24f5913fde3142967367779fe9516f7b01919aba
Josué Clément <josue.d.clement@gmail.com>
2025-12-15 14:45:09 +0100

    Add FontManager

---
+++ App1/FontManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace App1;
+
+public class FontManager(ContentManager content)
+{
+    private Dictionary<object, SpriteFont> _fonts = [];
+    
+    public void LoadFonts(params (object key, string assetName)[] assets)
+    {
+        foreach ((object key, string assetName) in assets)
+            _fonts.Add(key, content.Load<SpriteFont>(assetName));
+    }
+
+    public void DrawString(SpriteBatch spriteBatch, object key, string text, Vector2 position, Color color)
+        => spriteBatch.DrawString(_fonts[key], text, position, color);
+}
--- App1/Game1.cs
+++ App1/Game1.cs
@@ -4,28 +4,30 @@ using Microsoft.Xna.Framework.Input;
 
 namespace App1;
 
+public enum UserFont
+{
+    JetBrainsMonoRegular12,
+    JetBrainsMonoRegular14,
+    JetBrainsMonoRegular16,
+    JetBrainsMonoRegular18,
+    JetBrainsMonoRegular20,
+    JetBrainsMonoRegular24
+}
+
 public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private FontManager _fontManager;
     
-    private SpriteFont _font;
-
-    
-    // FPS tracking variables
-    private double _frames = 0;
-    private double _updates = 0;
-    private double _elapsed = 0;
-    private double _last = 0;
-    private double _now = 0;
-    private double _fps = 0;
-
+    private int _frames = 0;
 
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+        _fontManager = new FontManager(Content);
     }
 
     protected override void Initialize()
@@ -38,8 +40,14 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        _font = Content.Load<SpriteFont>("JetBrainsMono-Regular");
+        
+        _fontManager.LoadFonts(
+            (UserFont.JetBrainsMonoRegular12, "JetBrainsMono-Regular-12"),
+            (UserFont.JetBrainsMonoRegular14, "JetBrainsMono-Regular-14"),
+            (UserFont.JetBrainsMonoRegular16, "JetBrainsMono-Regular-16"),
+            (UserFont.JetBrainsMonoRegular18, "JetBrainsMono-Regular-18"),
+            (UserFont.JetBrainsMonoRegular20, "JetBrainsMono-Regular-20"),
+            (UserFont.JetBrainsMonoRegular24, "JetBrainsMono-Regular-24"));
     }
 
     protected override void Update(GameTime gameTime)
@@ -47,20 +55,6 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        // Calculate FPS
-        _now = gameTime.TotalGameTime.TotalSeconds;
-        _elapsed = _now - _last;
-            
-        if (_elapsed > 1)
-        {
-            _fps = _frames / _elapsed;
-            _elapsed = 0;
-            _frames = 0;
-            _last = _now;
-        }
-            
-        _updates++;
 
         base.Update(gameTime);
     }
@@ -71,9 +65,8 @@ public class Game1 : Game
 
         _spriteBatch.Begin();
             
-        // Draw FPS counter in top-left corner
-        string fpsText = $"FPS: {_frames:0.00}";
-        _spriteBatch.DrawString(_font, fpsText, new Vector2(10, 10), Color.White);
+        var frames = $"Frame: {_frames}";
+        _fontManager.DrawString(_spriteBatch, UserFont.JetBrainsMonoRegular24, frames, new Vector2(10, 10), Color.White);
             
         _spriteBatch.End();
 
