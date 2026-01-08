6f1ba7c3ce751d8ff5f70cbfd787f0c752531d19
Josué Clément <josue.d.clement@gmail.com>
2026-01-08 15:18:20 +0100

    Save changes

--- App1/Game1.cs
+++ App1/Game1.cs
@@ -21,8 +21,9 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private FontManager<UserFont> _fontManager;
     private Texture2D _pixel;
-    private int _width = 1920;
-    private int _height = 1080;
+    private int _width = 800;
+    private int _height = 600;
+    private TicTacToe _ticTacToe;
     
     private int _frames = 0;
 
@@ -59,6 +60,7 @@ public class Game1 : Game
         
         _pixel = new Texture2D(GraphicsDevice, 1, 1);
         _pixel.SetData([Color.White]);
+        _ticTacToe = new TicTacToe(_width, _height, _pixel);
     }
 
     protected override void Update(GameTime gameTime)
@@ -66,6 +68,8 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+        
+        _ticTacToe.Update();
 
         base.Update(gameTime);
     }
@@ -75,22 +79,11 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         _spriteBatch.Begin();
+        
+        _ticTacToe.Draw(_spriteBatch);
             
         var frames = $"Frame: {_frames}";
         _fontManager.DrawString(_spriteBatch, UserFont.JetBrainsMonoRegular24, frames, new Vector2(10, 10), Color.White);
-        
-        //TODO: do that in the right class
-        for (int i = 0; i < 2; i++)
-        {
-            float xCoord = _width / 3 * (i+1);
-            DrawLine(_spriteBatch, _pixel, new Vector2(xCoord, 0), new Vector2(xCoord, _height), Color.Black, 2f);
-        }
-
-        for (int i = 0; i < 2; i++)
-        {
-            float yCoord = _height / 3 * (i+1);
-            DrawLine(_spriteBatch, _pixel, new Vector2(0, yCoord), new Vector2(_width, yCoord), Color.Black, 2f);
-        }
             
         _spriteBatch.End();
 
@@ -99,21 +92,21 @@ public class Game1 : Game
         base.Draw(gameTime);
     }
     
-    public static void DrawLine(SpriteBatch spriteBatch, Texture2D pixel, Vector2 start, Vector2 end, Color color, float thickness = 1f)
-    {
-        float distance = Vector2.Distance(start, end);
-        float angle = (float)Math.Atan2(end.Y - start.Y, end.X - start.X);
-    
-        spriteBatch.Draw(
-            pixel,
-            start,
-            null,
-            color,
-            angle,
-            Vector2.Zero,
-            new Vector2(distance, thickness),
-            SpriteEffects.None,
-            0
-        );
-    }
+    // public static void DrawLine(SpriteBatch spriteBatch, Texture2D pixel, Vector2 start, Vector2 end, Color color, float thickness = 1f)
+    // {
+    //     float distance = Vector2.Distance(start, end);
+    //     float angle = (float)Math.Atan2(end.Y - start.Y, end.X - start.X);
+    //
+    //     spriteBatch.Draw(
+    //         pixel,
+    //         start,
+    //         null,
+    //         color,
+    //         angle,
+    //         Vector2.Zero,
+    //         new Vector2(distance, thickness),
+    //         SpriteEffects.None,
+    //         0
+    //     );
+    // }
 }
---
+++ App1/TicTacToe.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace App1;
+
+public class TicTacToe
+{
+    private readonly int _width;
+    private readonly int _height;
+    private readonly Texture2D _pixel;
+    private bool _mouseReleased = true;
+
+    public TicTacToe(int width, int height, Texture2D pixel)
+    {
+        _width = width;
+        _height = height;
+        _pixel = pixel;
+    }
+    
+    public int[,] Board { get; set; } = new int[3, 3];
+
+    public void Update()
+    {
+        var mouseState = Mouse.GetState();
+        var mousePosition = new Vector2(mouseState.X, mouseState.Y);
+        
+        if (mouseState.LeftButton == ButtonState.Released)
+            _mouseReleased = true;
+
+        if (!_mouseReleased)
+            return;
+        
+        if (mouseState.LeftButton == ButtonState.Pressed)
+        {
+            _mouseReleased = false;
+            var xCell = (int)Math.Floor(mousePosition.X / _width * 3);
+            var yCell = (int)Math.Floor(mousePosition.Y / _height * 3);
+
+            if (xCell is >= 0 and < 3 && yCell is >= 0 and < 3)
+            {
+                if (Board[xCell, yCell] == 0)
+                    Board[xCell, yCell] = 1;
+                else
+                    Board[xCell, yCell] = 0;
+                // Console.WriteLine($"{xCell}, {yCell}");
+            }
+        }
+        
+        
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            float xCoord = _width / 3 * (i+1);
+            DrawLine(spriteBatch, _pixel, new Vector2(xCoord, 0), new Vector2(xCoord, _height), Color.Black, 2f);
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            float yCoord = _height / 3 * (i+1);
+            DrawLine(spriteBatch, _pixel, new Vector2(0, yCoord), new Vector2(_width, yCoord), Color.Black, 2f);
+        } 
+        
+        for (int i = 0; i < Board.GetLength(0); i++)
+        {
+            for (int j = 0; j < Board.GetLength(1); j++)
+            {
+                if (Board[i, j] != 0)
+                {
+                    spriteBatch.Draw(_pixel, new Vector2(i * _width / 3, j * _height / 3), null, Color.Black, 0f, Vector2.Zero, new Vector2(_width / 3, _height / 3), SpriteEffects.None, 0f);
+                }
+            }
+        } 
+    }
+    
+    public static void DrawLine(SpriteBatch spriteBatch, Texture2D pixel, Vector2 start, Vector2 end, Color color, float thickness = 1f)
+    {
+        float distance = Vector2.Distance(start, end);
+        float angle = (float)Math.Atan2(end.Y - start.Y, end.X - start.X);
+    
+        spriteBatch.Draw(
+            pixel,
+            start,
+            null,
+            color,
+            angle,
+            Vector2.Zero,
+            new Vector2(distance, thickness),
+            SpriteEffects.None,
+            0
+        );
+    }
+}
